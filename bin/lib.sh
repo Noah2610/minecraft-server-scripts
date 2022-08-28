aad774903a8056a3216c5de0537de690381297d3
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2022-08-28 02:09:41 +0200

    Format lib.sh, ignore jar files

--- bin/lib.sh
+++ bin/lib.sh
@@ -38,25 +38,25 @@ unset logfile_dir
 
 # Prints the current date to the logfile (and stdout).
 function print_date {
-  msg "--- $( date "+%F %T" ) ---"
+    msg "--- $( date "+%F %T" ) ---"
 }
 
 # Returns 0 if the server is running.
 function is_server_running {
-  # TODO: Properly check if the actual java server app is running.
-  tmux has-session -t $TMUX_SESSION &> /dev/null
+    # TODO: Properly check if the actual java server app is running.
+    tmux has-session -t $TMUX_SESSION &> /dev/null
 }
 
 # Runs the given command on the server, if it is running.
 function server_cmd {
-  is_server_running && \
-    tmux send-keys -t "$TMUX_PANE" Enter "$1" Enter
-    # Start command with an Enter press, to ensure nothing has been typed previously (hacky).
-}
+    is_server_running && \
+        tmux send-keys -t "$TMUX_PANE" Enter "$1" Enter
+            # Start command with an Enter press, to ensure nothing has been typed previously (hacky).
+        }
 
 # Write a message to the minecraft server by sending keystrokes to the correct tmux session/window/pane.
 function print_chat {
-  server_cmd "say ${1//$'\n'/$'\n'say }"
+    server_cmd "say ${1//$'\n'/$'\n'say }"
 }
 
 # Verify that some programs are available.
